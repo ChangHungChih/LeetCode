c69d5b2cc4a9f608c0f8ec39b56b2486acc22c55
Sean <seanchang@lafresh.com.tw>
2021-03-09 20:43:03 +0800

    623. Add One Row to Tree

---
+++ src/main/kotlin/623 Add One Row to Tree.kt
@@ -0,0 +1,36 @@
+/**
+ * https://leetcode.com/problems/add-one-row-to-tree/
+ * */
+
+//Runtime: 192 ms, faster than 66.67% of Kotlin online submissions for Add One Row to Tree.
+//Memory Usage: 36.3 MB, less than 100.00% of Kotlin online submissions for Add One Row to Tree.
+fun addOneRow(root: TreeNode?, v: Int, d: Int): TreeNode? {
+    if (d == 1) {
+        return TreeNode(v).apply {
+            left = root
+        }
+    }
+    addNode(root, v, d)
+    return root
+}
+
+fun addNode(root: TreeNode?, v: Int, d: Int, isLeft: Boolean = true) {
+    root?.apply {
+        if (d == 2) {
+            root.left = TreeNode(v).apply {
+                left = root.left
+            }
+            root.right = TreeNode(v).apply {
+                right = root.right
+            }
+        } else {
+            addNode(root.left, v, d - 1)
+            addNode(root.right, v, d - 1, false)
+        }
+    }
+}
+
+class TreeNode(var `val`: Int) {
+    var left: TreeNode? = null
+    var right: TreeNode? = null
+}
